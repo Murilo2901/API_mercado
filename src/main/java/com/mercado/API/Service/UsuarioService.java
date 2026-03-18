49ed10e98c2f62b1d7fdd99aabb4a4e1b8c37ef7
MURILO KERSCHBAUM <murilo_kerschbaum@estudante.sesisenai.org.br>
2026-03-18 20:16:37 -0300

    feat:implementando a lógica de negócio do UsuarioService

--- src/main/java/com/mercado/API/Service/UsuarioService.java
+++ src/main/java/com/mercado/API/Service/UsuarioService.java
@@ -1,4 +1,21 @@
 package com.mercado.API.Service;
 
+import com.mercado.API.DAO.UsuarioDAO;
+import com.mercado.API.Domain.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
 public class UsuarioService {
+    @Autowired
+    private UsuarioDAO usuarioDAO;
+
+    public Usuario salvar(Usuario usuario){
+        return usuarioDAO.save(usuario);
+    }
+
+    public List<Usuario> buscarTodos(){
+        return usuarioDAO.findAll();
+    }
 }
