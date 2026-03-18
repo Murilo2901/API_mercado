12aeba18a28c3de70627cf8e73f8b9825e625701
CATARINA KLEIN <catarina_klein@estudante.sesisenai.org.br>
2026-03-18 20:18:45 -0300

    feat: cria os endpoints de produto

--- src/main/java/com/mercado/API/Controller/ProdutoController.java
+++ src/main/java/com/mercado/API/Controller/ProdutoController.java
@@ -1,4 +1,36 @@
 package com.mercado.API.Controller;
 
+
+import com.mercado.API.Domain.Produto;
+import com.mercado.API.Service.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/produtos")
 public class ProdutoController {
+@Autowired
+    private ProdutoService produtoService;
+
+@PostMapping
+    public Produto cadastrar(@RequestBody Produto produto){
+    return produtoService.salvar(produto);
+}
+
+@GetMapping
+    public List<Produto> listarTodos(){
+    return produtoService.buscarTodos();
+}
+
+@PutMapping("/{id}")
+    public Produto atualizar(@PathVariable Long id, @RequestBody Produto produto){
+    return produtoService.atualizar(id, produto);
+}
+
+@DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id){
+    produtoService.deletar(id);
+}
 }
