6b70cd64930a3882fb5b936037c6b8977ccb6c34
MURILO KERSCHBAUM <murilo_kerschbaum@estudante.sesisenai.org.br>
2026-03-18 20:53:02 -0300

    feat:implementando a lógica de negócio da VendaService

--- src/main/java/com/mercado/API/Service/VendaService.java
+++ src/main/java/com/mercado/API/Service/VendaService.java
@@ -1,4 +1,39 @@
 package com.mercado.API.Service;
 
+import com.mercado.API.DAO.ProdutoDAO;
+import com.mercado.API.DAO.VendaDAO;
+import com.mercado.API.Domain.Produto;
+import com.mercado.API.Domain.Venda;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
 public class VendaService {
+    @Autowired private VendaDAO vendaDAO;
+    @Autowired private ProdutoDAO produtoDAO;
+
+    @Transactional
+    public Venda registrarVenda(Venda venda){
+        Produto produto = produtoDAO.findById(venda.getProduto().getId())
+                .orElseThrow(() -> new RuntimeException("Produto não encontrado!"));
+        if(produto.getQuantidadeEstoque() <venda.getQuantidadeComprada()){
+            throw new RuntimeException("Estoque insuficiente!");
+        }
+
+        produto.setQuantidadeEstoque(produto.getQuantidadeEstoque() -venda.getQuantidadeComprada());
+        produtoDAO.save(produto);
+
+        venda.setDataVenda(LocalDate.now());
+        venda.setStatusVenda("CONCLUIDA");
+        return vendaDAO.save(venda);
+    }
+
+    public List<Venda> buscarVendasDoUsuario(Long usuarioId){
+        return vendaDAO.findByUsuarioId(usuarioId);
+    }
+
 }
