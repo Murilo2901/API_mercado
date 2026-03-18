a96b272a5691a56150547d71dea2528a13bf9c74
Lucas Schlei <lucas_schlei@estudante.sesisenai.org.br>
2026-03-18 20:54:19 -0300

    Feat:Implementando a classe ProdutoDAO

--- src/main/java/com/mercado/API/DAO/ProdutoDAO.java
+++ src/main/java/com/mercado/API/DAO/ProdutoDAO.java
@@ -1,4 +1,6 @@
 package com.mercado.API.DAO;
 
-public class ProdutoDAO {
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.mercado.API.model.Produto;
+public interface ProdutoDAO extends JpaRepository<Produto, Long> {
 }
