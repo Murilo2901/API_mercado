3af061a346b4955d7027f8a816eaafbd8a8fe583
CATARINA KLEIN <catarina_klein@estudante.sesisenai.org.br>
2026-03-18 20:44:38 -0300

    feat: cria os endpoints de usuario

--- src/main/java/com/mercado/API/Controller/UsuarioController.java
+++ src/main/java/com/mercado/API/Controller/UsuarioController.java
@@ -1,4 +1,25 @@
 package com.mercado.API.Controller;
 
+import com.mercado.API.Domain.Usuario;
+import com.mercado.API.Service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/usuarios")
 public class UsuarioController {
+@Autowired
+    private UsuarioService usuarioService;
+
+@PostMapping
+    public Usuario cadastrar(@RequestBody Usuario usuario){
+    return usuarioService.salvar(usuario);
+}
+
+@GetMapping
+    public List<Usuario> listarTodos(){
+    return usuarioService.buscarTodos();
+}
 }
