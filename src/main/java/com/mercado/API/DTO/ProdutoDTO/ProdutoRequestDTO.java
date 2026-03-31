fda57d66363d5e35c15d38e19d8cc9e3c5cb167c
LUCAS SCHLEI <lucas_schlei@estudante.sesisenai.org.br>
2026-03-30 21:30:23 -0300

    Feat:Fazendo a implementacao da classe DTO no projeto.

---
+++ src/main/java/com/mercado/API/DTO/ProdutoDTO/ProdutoRequestDTO.java
@@ -0,0 +1,43 @@
+package com.mercado.API.DTO.ProdutoDTO;
+
+
+import java.math.BigDecimal;
+
+public class ProdutoRequestDTO {
+    private String nome;
+    private String categoria;
+    private BigDecimal preco;
+    private Integer quantidadeEstoque;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public Integer getQuantidadeEstoque() {
+        return quantidadeEstoque;
+    }
+
+    public void setQuantidadeEstoque(Integer quantidadeEstoque) {
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+}
