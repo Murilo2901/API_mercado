0fc8cbf1dfcbeea4b83a47d65718d48e5dd1966e
MURILO KERSCHBAUM <murilo_kerschbaum@estudante.sesisenai.org.br>
2026-04-01 15:45:09 -0300

    feat:corrigindo o DTO

--- src/main/java/com/mercado/API/DTO/UsuarioDTO/UsuarioResponseDTO.java
+++ src/main/java/com/mercado/API/DTO/UsuarioDTO/UsuarioResponseDTO.java
@@ -1,12 +1,18 @@
 package com.mercado.API.DTO.UsuarioDTO;
 
 public class UsuarioResponseDTO {
+    private boolean sucesso = true;
+    private String mensagem = "Cadastro realizado com sucesso!";
     private Long id;
     private String nome;
     private String email;
     private String usuario;
     private String perfil;
 
+    public boolean isSucesso() { return sucesso; }
+    public void setSucesso(boolean sucesso) { this.sucesso = sucesso; }
+    public String getMensagem() { return mensagem; }
+    public void setMensagem(String mensagem) { this.mensagem = mensagem; }
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
     public String getNome() { return nome; }
