b6473b133db1451fc81614e88a1cdc6cc3de04b9
MURILO KERSCHBAUM <murilo_kerschbaum@estudante.sesisenai.org.br>
2026-04-01 14:27:30 -0300

    feat:corrigindo o repository

--- src/main/java/com/mercado/API/Repository/ProdutoRepository.java
+++ src/main/java/com/mercado/API/Repository/ProdutoRepository.java
@@ -2,6 +2,11 @@ package com.mercado.API.Repository;
 
 import com.mercado.API.Model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
 
+@Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+    List<Produto> findByCategoriaAndAtivoTrue(String categoria);
+    List<Produto> findByAtivoTrue();
 }
